855a414ef92959123de7de943f4872d5227cbaee
homrajkc <homrajkc53@gmail.com>
2025-02-05 21:05:42 +0545

    first commit

---
+++ Game.cpp
@@ -0,0 +1,43 @@
+
+#include "Game.hpp"
+#include <SFML/Graphics/CircleShape.hpp>
+
+#include "MainMenu.hpp"
+
+Game::Game():m_context(std::make_shared<Context>())
+ {
+     m_context->m_window->create(sf::VideoMode(900, 600), "Snake Game",sf::Style::Close);
+     m_context->m_states->Add(std::make_unique<MainMenu>(m_context));
+ }
+Game::~Game()
+{
+
+}
+
+void Game::Run()
+{
+   sf::CircleShape shape(100.f);
+   shape.setFillColor(sf::Color::Green);
+
+   sf::Clock clock;
+   sf::Time timeSinceLastFrame=sf::Time::Zero;
+
+
+   while (m_context->m_window->isOpen())
+   {
+       timeSinceLastFrame+= clock.restart();
+
+
+       while(timeSinceLastFrame > TIME_PER_FRAME)
+       {
+        timeSinceLastFrame-=TIME_PER_FRAME;
+
+       m_context->m_states->ProcessStateChange();
+       m_context->m_states->Getcurrent()->ProcessInput();
+       m_context->m_states->Getcurrent()->Update(TIME_PER_FRAME);
+       m_context->m_states->Getcurrent()->Draw();
+
+
+       }
+   }
+}
